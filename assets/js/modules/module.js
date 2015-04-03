e7b77ea60c259b6e15c476611b4d4d44ec75e0ef
Matthieu Colle <matthieu@johnmcneilstudio.com>
2015-04-03 13:21:23 -0700

    [FEATURE] Added first commands

--- assets/js/modules/module.js
+++ assets/js/modules/module.js
@@ -2,8 +2,14 @@
 
 'use strict';
 
+var is = require('is_js');
+
 module.exports = {
   init: function(name) {
-    return 'Hi, '+name;
+    this.name = name;
+    return 'Hi, '+this.name;
+  },
+  areYouString: function() {
+    return is.string(this.name);
   }
 };
